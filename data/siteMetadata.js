18fafee8a37c0a3212d088477b10f00814faa37c
Shahadat Hossen <shahadat.07.sh@gmail.com>
2023-09-26 08:17:12 +0600

    banner changed

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Shahadat Hossen',
   author: 'Shahadat Hossen',
   headerTitle: 'Shahadat Hossen',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Personal Portfolio',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -11,12 +11,12 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
+  email: 'shahadat.hossen.web@gmail.com',
+  github: 'https://github.com/shahadat-07',
+  twitter: 'https://twitter.com/shahadat_webdev',
+  facebook: 'https://web.facebook.com/shahadat.web.dev',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/shahadat-hossen-07/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
